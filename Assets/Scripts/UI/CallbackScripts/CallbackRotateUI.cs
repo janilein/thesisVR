05f3cc2fcdce39af07d2a0024568a302c538f4a7
janilein <jan.hardy95@gmail.com>
2017-11-13 17:31:17 +0100

    Changes to rotation UI

--- Assets/Scripts/UI/CallbackScripts/CallbackRotateUI.cs
+++ Assets/Scripts/UI/CallbackScripts/CallbackRotateUI.cs
@@ -1,47 +1,94 @@
 ﻿namespace callback {
-
+    using System;
+    using System.Collections;
     using UnityEngine;
     using UnityEngine.UI;
 
     public class CallbackRotateUI : CallbackRotate {
 
-        private Text degreesText;
-        private Slider slider;
-        private float degrees;
+        private Text degreesTextRoll, degreesTextPitch, degreesTextYaw;
+        private Slider sliderPitch, sliderYaw, sliderRoll;
+        Vector3 originalPitchVector, previousPitch, originalYawVector, previousYaw, originalRollVector, previousRoll;
 
         private void Awake() {
             Manager = GameObject.Find("GUI").transform.GetComponent<menumanager.UIManager>();
-            degreesText = GameObject.Find("GUI/Canvas/RotateButtons/Text").GetComponent<Text>();
-            slider = GameObject.Find("GUI/Canvas/RotateButtons/Slider").GetComponent<Slider>();
+            
+            sliderPitch = GameObject.Find("GUI/Canvas/RotateButtons/SliderPitch/Slider").GetComponent<Slider>();
+            sliderRoll = GameObject.Find("GUI/Canvas/RotateButtons/SliderRoll/Slider").GetComponent<Slider>();
+            sliderYaw = GameObject.Find("GUI/Canvas/RotateButtons/SliderYaw/Slider").GetComponent<Slider>();
+
+            degreesTextPitch = GameObject.Find("GUI/Canvas/RotateButtons/SliderPitch/Text").GetComponent<Text>();
+            degreesTextRoll = GameObject.Find("GUI/Canvas/RotateButtons/SliderRoll/Text").GetComponent<Text>();
+            degreesTextYaw = GameObject.Find("GUI/Canvas/RotateButtons/SliderYaw/Text").GetComponent<Text>();
+            originalPitchVector = previousPitch = new Vector3(0f, 0f, 0f);
         }
 
-        public void SliderValueChanged() {
-            degreesText.text = slider.value + " degrees";
-            degrees = slider.value;
+        public void SliderYawChanged() {
+            if (!sliderYaw) {
+                Debug.Log("Could not find the attached yaw slider");
+            } else {
+                degreesTextYaw.text = sliderYaw.value + " degree(s)";
+                StartCoroutine("Yaw");
+            }
         }
 
-        public void PitchObjectLeft() {
-            ObjectManager.RotateObject(new Vector3(0f, degrees, 0f));
+        public void SliderRollChanged() {
+            if (!sliderRoll) {
+                Debug.Log("Could not find the attached roll slider");
+            } else {
+                degreesTextRoll.text = sliderRoll.value + " degree(s)";
+                StartCoroutine("Roll");
+            }
         }
 
-        public void PitchObjectRight() {
-            ObjectManager.RotateObject(new Vector3(0f, -degrees, 0f));
+        public void SliderPitchChanged() {
+            if (!sliderPitch) {
+                
+                Debug.Log("Could not find the attached pitch slider");
+            } else {
+                degreesTextPitch.text = sliderPitch.value + " degree(s)";
+                StartCoroutine("Pitch");
+            }
         }
 
-        public void RollObjectLeft() {
-            ObjectManager.RotateObject(new Vector3(degrees, 0f, 0f));
+        private IEnumerator Pitch() {
+            while (true) {
+                originalPitchVector = Vector3.Lerp(previousPitch, new Vector3(sliderPitch.value, 0f, 0f), Time.deltaTime);
+                ObjectManager.RotateObject(originalPitchVector - previousPitch);
+                previousPitch = originalPitchVector;
+
+                    if (Math.Abs(sliderPitch.value - previousPitch.x) < 0.01f) {
+                        StopCoroutine("Pitch");
+                    }
+                yield return null;
+            }
         }
 
-        public void RollObjectRight() {
-            ObjectManager.RotateObject(new Vector3(-degrees, 0f, 0f));
+        private IEnumerator Roll() {
+            while (true) {
+                originalRollVector = Vector3.Lerp(previousRoll, new Vector3(0f, 0f, sliderRoll.value), Time.deltaTime);
+                ObjectManager.RotateObject(originalRollVector - previousRoll);
+                previousRoll = originalRollVector;
+
+                    if (Math.Abs(sliderRoll.value - previousRoll.z) < 0.01f) {
+                        StopCoroutine("Roll");
+
+                    }
+                yield return null;
+            }
         }
 
-        public void YawObjectLeft() {
-            ObjectManager.RotateObject(new Vector3(0f, 0f, degrees));
-        }
+        private IEnumerator Yaw() {
+            while (true) {
+                originalYawVector = Vector3.Lerp(previousYaw, new Vector3(0f, sliderYaw.value, 0f), Time.deltaTime);
+                ObjectManager.RotateObject(originalYawVector - previousYaw);
+                previousYaw = originalYawVector;
 
-        public void YawObjectRight() {
-            ObjectManager.RotateObject(new Vector3(0f, 0f , -degrees));
+                    if (Math.Abs(sliderYaw.value - previousYaw.y) < 0.01f) {
+                        StopCoroutine("Yaw");
+                    }
+                yield return null;
+            }
         }
 
         public void Back() {

e71307f4481646119a87eb3971821592d9b87859
janilein <jan.hardy95@gmail.com>
2018-02-15 17:02:38 +0100

    added json keyword extraction from the meaningcloud reply

--- Assets/Scripts/Speech/KeywordParser.cs
+++ Assets/Scripts/Speech/KeywordParser.cs
@@ -1,16 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
-public class KeywordParser : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class KeywordParser {
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ConvertHashtable(Hashtable o) {
+        foreach (DictionaryEntry entry in o) {
+            if (entry.Key.Equals("entity_list")) {
+                CheckEntityList((ArrayList)entry.Value);
+            } else if (entry.Key.Equals("concept_list")) {
+                CheckConceptList((ArrayList)entry.Value);
+            } else if (entry.Key.Equals("quantity_expression_list")) {
+                CheckQuantityExpressionList((ArrayList)entry.Value);
+            }
+        }
+    }
+
+    private void CheckQuantityExpressionList(ArrayList list) {
+        if (list.Count == 0) {
+            Debug.Log("This list is empty");
+        } else {
+            foreach(DictionaryEntry entry in (Hashtable)list[0]) {
+                if (entry.Key.Equals("unit")) {
+                    Debug.Log((string)entry.Value);
+                }
+                if (entry.Key.Equals("amount_form")) {
+                    Debug.Log((string)entry.Value);
+                }
+            }
+        }
+    }
+
+    private void CheckConceptList(ArrayList list) {
+        if (list.Count == 0) {
+            Debug.Log("This list is empty");
+        } else {
+            foreach (Hashtable table in list) {
+                foreach (DictionaryEntry entry in table) {
+                    //Debug.Log("Key: " + entry.Key + " Value: " + entry.Value.ToString());
+                    if (entry.Key.Equals("form")) {
+                        Debug.Log((string)entry.Value);
+                    }
+                    if (entry.Key.Equals("sementity")) {
+                        Debug.Log(GetTypeOfKeyword((Hashtable)entry.Value));
+                    }
+                }
+            }
+        }
+    }
+
+    private void CheckEntityList(ArrayList list) {
+        if(list.Count == 0) {
+            Debug.Log("This list is empty");
+        } else {
+            foreach (DictionaryEntry entry in (Hashtable)list[0]) {
+
+            }
+        }
+    }
+
+    private string GetTypeOfKeyword(Hashtable value) {
+        foreach (DictionaryEntry entry in value) {
+            if (entry.Key.Equals("type")) {
+                string type = (string)entry.Value;
+                type = type.Substring(4, type.Length - 4);
+                return type;
+            }
+        }
+        return null;
+    }
 }

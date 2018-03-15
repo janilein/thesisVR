2ed4ae36a08866f435e2b788175ec1ba71b349b0
JasperVandenberghe <jasper.vandenberghe@hotmail.com>
2018-03-15 17:11:37 +0100

    Literally finished the entire thesis

---
+++ Assets/Scripts/SelectableScripts/SelectableProcedural.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableProcedural : SelectableObject {
+
+    public override void HighlightObject()
+    {
+        Debug.Log("enable halo");
+        Component halo = GetComponent("Halo");
+        halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
+    }
+
+    public override void RemoveHighlight()
+    {
+        Debug.Log("disable halo");
+        Component halo = GetComponent("Halo");
+        halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+    }
+}
